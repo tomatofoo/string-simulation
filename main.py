5ba2664a5c1a5507877bdeeeb713683d9440aad9
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-04-13 02:03:08 -0700

    add fluff

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class Particle(object):
 
         self._mass = mass
         self._pos = pg.Vector2(pos)
+        self._prev = self._pos.copy()
         self._velocity = pg.Vector2(velocity)
         self._net_force = pg.Vector2(net_force)
         self._accel = self._net_force / self._mass
@@ -87,19 +88,13 @@ class Particle(object):
         self._pos += self._velocity * rel_game_speed
         self._velocity *= self._friction**rel_game_speed
 
-        """
-        self._velocity += 0.5 * self._accel * rel_game_speed
-        self._pos += self._velocity * rel_game_speed
-        self._accel = self._net_force / self._mass
-        self._velocity += 0.5 * self._accel * rel_game_speed
-        """
-
 
 class Game(object):
 
     _SCREEN_SIZE = (640, 480)
     _SCREEN_FLAGS = pg.RESIZABLE | pg.SCALED
     _GAME_SPEED = 1
+    _TIMESTEP = 1 / 60
 
     def __init__(self: Self) -> None:
         pg.init()
@@ -113,19 +108,24 @@ class Game(object):
             vsync=self._settings['vsync']
         )
         self._running = 0
-        
+
+
         # RENDERING
         self._RADIUS = 4
+        self._THICKNESS = 4
         self._COLORS = {
             'fill': (0, 0, 0),
             'arm': (255, 0, 0),
             'bob': (255, 255, 255),
             'pivot': (0, 255, 0),
+            'text': (255, 255, 255),
         }
-        
+        self._SHOW_BOBS = 0 # not on purpose i swear
+
         # PHYSICS
+        self._MOVEMENT = 120 # multiplier of mouse cursor movement
         self._GRAVITY = pg.Vector2(0, 1000)
-        self._AMOUNT = 10 # amount of pendulums
+        self._AMOUNT = 12 # amount of pendulums
         self._LENGTH = 100 / self._AMOUNT
         self._MASS = 1
         self._K = 1000
@@ -134,29 +134,68 @@ class Game(object):
         self._pivot = Particle(1, pg.Vector2(self._SCREEN_SIZE) / 2)
         self._reset()
 
+        self._font = pg.font.SysFont('Arial', 16)
+        self._info = self._font.render(
+            '<r> to reset the rope\n'
+            '<s> to show / hide spring bobs\n'
+            'drag pivot with mouse cursor',
+            1,
+            self._COLORS['text'],
+        )
+
     def _reset(self: Self) -> None:
-        vector = pg.Vector2(self._LENGTH, 0).rotate(45)
+        vector = pg.Vector2(self._LENGTH, 0)
         self._bobs = []
         for i in range(self._AMOUNT):
             bob = Particle(
                 mass=self._MASS,
                 pos=self._pivot.pos + (i + 1) * vector,
-                friction=0.8,
+                friction=0.4,
             )
             self._bobs.append(bob)
+
+    def _render(self: Self, accumulator: Real) -> None:
+        self._screen.fill(self._COLORS['fill'])
+        prev = self._pivot.pos
+        for bob in self._bobs:
+            # Interpolation
+            pos = bob.pos + bob.velocity * accumulator
+            pg.draw.line(
+                self._screen,
+                self._COLORS['arm'],
+                prev,
+                pos,
+                self._THICKNESS,
+            )
+            if self._SHOW_BOBS:
+                pg.draw.circle(
+                    self._screen,
+                    self._COLORS['bob'],
+                    pos,
+                    self._RADIUS,
+                )
+            prev = pos
+        pg.draw.circle(
+            self._screen,
+            self._COLORS['pivot'],
+            self._pivot.pos,
+            self._RADIUS,
+        )
 
     def run(self: Self) -> None:
         self._running = 1
         start_time = time.time()
+        accumulator = 0 # https://www.gafferongames.com/post/fix_your_timestep/
 
         clicking = 0
+        movement = (0, 0)
 
         while self._running:
             delta_time = time.time() - start_time
             start_time = time.time()
 
             rel_game_speed = delta_time * self._GAME_SPEED
-            
+
             # EVENTS
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -164,54 +203,48 @@ class Game(object):
                 elif event.type == pg.KEYDOWN:
                     if event.key == pg.K_r:
                         self._reset()
+                    elif event.key == pg.K_s:
+                        self._SHOW_BOBS = not self._SHOW_BOBS
                 elif event.type == pg.MOUSEMOTION and clicking:
                     self._pivot.pos += event.rel
-                elif event.type == pg.MOUSEBUTTONDOWN:
-                    clicking = self._pivot.pos.distance_to(event.pos) < self._RADIUS
+                    movement = pg.Vector2(event.rel) * self._MOVEMENT
+                elif (
+                    event.type == pg.MOUSEBUTTONDOWN
+                    and self._pivot.pos.distance_to(event.pos) < self._RADIUS
+                ):
+                    clicking = 1
                 elif event.type == pg.MOUSEBUTTONUP:
                     clicking = 0
-
-            pg.display.set_caption(str(1 / delta_time) if delta_time else 'inf')
             
             # UPDATE
-            force = pg.Vector2(0, 0)
-            for i in range(self._AMOUNT - 1, -1, -1):
-                bob = self._bobs[i]
-                next = self._bobs[i - 1] if i else self._pivot
-                spring = bob.pos - next.pos
-                restoring = ( # F = -kx
-                    -self._K
-                    * (spring.magnitude() - self._LENGTH)
-                    * spring.normalize()
-                )
-                bob.net_force = self._GRAVITY * bob.mass + restoring - force
-                force = restoring
-                bob.update(rel_game_speed)
+            accumulator += rel_game_speed
+            while accumulator > self._TIMESTEP:
+                rel_game_speed = self._TIMESTEP
+                accumulator -= rel_game_speed
 
+                # https://www.youtube.com/watch?v=0WaDxYuD9S8
+                antirestoring = pg.Vector2(0, 0)
+                for i in range(self._AMOUNT - 1, -1, -1):
+                    bob = self._bobs[i]
+                    next = self._bobs[i - 1] if i else self._pivot
+                    spring = bob.pos - next.pos
+                    restoring = ( # F = -kx
+                        -self._K
+                        * (spring.magnitude() - self._LENGTH)
+                        * spring.normalize()
+                    )
+                    bob.net_force = (
+                        movement
+                        + self._GRAVITY * bob.mass
+                        + restoring
+                        + antirestoring
+                    )
+                    antirestoring = -restoring
+                    bob.update(rel_game_speed)
+           
             # RENDER
-            self._screen.fill(self._COLORS['fill'])
-            pg.draw.circle(
-                self._screen,
-                self._COLORS['pivot'],
-                self._pivot.pos,
-                self._RADIUS,
-            )
-            prev = self._pivot
-            for bob in self._bobs:
-                pg.draw.line(
-                    self._screen,
-                    self._COLORS['arm'],
-                    prev.pos,
-                    bob.pos,
-                )
-                pg.draw.circle(
-                    self._screen,
-                    self._COLORS['bob'],
-                    bob.pos,
-                    self._RADIUS,
-                )
-                prev = bob
-            
+            self._render(accumulator)
+            self._screen.blit(self._info, (0, 0))
             pg.display.update()
 
         pg.quit()
